ddd2e1c5f34ac2c5b15db710550658f537c5e067
dhdydtn91 <53810412+dhdydtn91@users.noreply.github.com>
2019-08-23 02:00:49 +0000

    Update Resource.java

--- src/util/Resource.java
+++ src/util/Resource.java
@@ -1,13 +1,9 @@
 package util;
 
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Scanner;
-
 import model.Dao;
 
 public interface Resource {
-	//StringBuffer loginID= new StringBuffer();
+	
 	Dao dao = new Dao();
-	//그럼해쉬체이블도같지 
+	
 }
